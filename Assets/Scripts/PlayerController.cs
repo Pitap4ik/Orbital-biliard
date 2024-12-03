b7f54f5a2679804403e85d2d32844354fd938761
Pitap4ik <potapodan0702@gmail.com>
2024-12-03 17:42:42 +0100

    spinnning around black hole

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System;
+using UnityEngine.InputSystem.Controls;
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,12 +8,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _initialY;
     [SerializeField] private float _constant1;
     [SerializeField] private float _constant2;
+    [SerializeField] private float VelocityX;
+    [SerializeField] private float VelocityY;
     private float _constant3;
     private Vector2 _initialVelocity;
     public Transform Transform { get; private set; }
     public Rigidbody2D Rigidbody { get; private set; }
-    public float VelocityX { get; private set; }
-    public float VelocityY { get; private set; } 
+    //public float VelocityX { get; private set; }
+    //public float VelocityY { get; private set; } 
     
     void Start()
     {
@@ -22,10 +25,10 @@ public class PlayerController : MonoBehaviour
         Vector2 initialPosition = new Vector2(_initialX, _initialY);
         Transform.position = initialPosition;
 
-        _initialVelocity = new Vector2(MathF.Sqrt(_constant1 * (_constant2 - 1 / GetDistance(initialPosition))), 0f);
-        _constant3 = _initialVelocity.x * GetDistance(_initialVelocity);
+        _initialVelocity = new Vector2(VelocityX, VelocityY);
+          _constant3 = _initialVelocity.x * GetDistance(Transform.position);
 
-        Debug.Log(_constant3);
+        Debug.Log(_initialVelocity.x);
     }
 
     void FixedUpdate()
@@ -33,20 +36,34 @@ public class PlayerController : MonoBehaviour
         float currentX = Transform.position.x;
         float currentY = Transform.position.y;
         float distance = GetDistance(Transform.position);
+        float sinB = -currentX / distance;
+        float cosB = currentY / distance * -1;
 
-        float velocity = MathF.Sqrt(_constant1 * (_constant2 - 1 / distance));
-        float cosA = _constant3 / (distance * velocity);
+        /*float velocity = MathF.Sqrt(_constant1 * (-1/_constant2 +1 / distance));
+        float cosA = _constant3 / (distance*sinB * velocity);
+        if (cosA > 1f) { cosA= 1f; }
+        if (cosA < -1f) { cosA = -1f; }
         float sinA = MathF.Sqrt(1 - MathF.Pow(cosA, 2));
-        float cosB = currentX/distance;
-        float sinB = currentY/distance*-1;
+
+        if (sinB < 0f) { sinA = -sinA; }
+        if (cosB < 0f) { cosA = -cosA; }
+
         float sinAB = sinA * cosB + sinB * cosA;
         float cosAB = MathF.Sqrt(1 - MathF.Pow(sinAB, 2));
-        
+
         VelocityX = velocity * sinAB;
-        VelocityY = velocity * cosAB;
+        VelocityY = velocity * cosAB;*/
+
+        float dT = Time.deltaTime;
+
+        VelocityX +=(_constant1/ MathF.Pow(distance, 2)) *dT* sinB;
+
+        VelocityY += (_constant1 / MathF.Pow(distance, 2)) * dT*cosB;
+
+
         Rigidbody.linearVelocity = new Vector2(VelocityX, VelocityY);
 
-        Debug.Log(cosA);
+        //Debug.Log(velocity +","+  currentX+ ","+ cosA); 
     }
 
     public float GetDistance(Vector2 pos){
