6ee72c40fee9bc28aa686966c2803ef22681f1ef
Karelius <vlastitel2006@gmail.com>
2024-12-04 19:10:25 +0100

    Hotfix 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,8 +12,6 @@ public class PlayerController : MonoBehaviour
     private float _constant3;
     public Transform Transform { get; private set; }
     public Rigidbody2D Rigidbody { get; private set; }
-    //public float VelocityX { get; private set; }
-    //public float VelocityY { get; private set; } 
     
     void Start()
     {
@@ -36,35 +34,17 @@ public class PlayerController : MonoBehaviour
         float sinB = -currentX / distance;
         float cosB = currentY / distance * -1;
 
-        /*float velocity = MathF.Sqrt(_constant1 * (-1/_constant2 +1 / distance));
-        float cosA = _constant3 / (distance*sinB * velocity);
-        if (cosA > 1f) { cosA= 1f; }
-        if (cosA < -1f) { cosA = -1f; }
-        float sinA = MathF.Sqrt(1 - MathF.Pow(cosA, 2));
-
-        if (sinB < 0f) { sinA = -sinA; }
-        if (cosB < 0f) { cosA = -cosA; }
-
-        float sinAB = sinA * cosB + sinB * cosA;
-        float cosAB = MathF.Sqrt(1 - MathF.Pow(sinAB, 2));
-
-        VelocityX = velocity * sinAB;
-        VelocityY = velocity * cosAB;*/
-
         float dT = Time.deltaTime;
-<<<<<<< Updated upstream
         _velocity.x += (_constant1/MathF.Pow(distance, 2))*dT*sinB*k;
         _velocity.y += (_constant1/MathF.Pow(distance, 2))*dT*cosB*k;
-=======
-        VelocityX += (_constant1/MathF.Pow(distance, 2))*dT*sinB*k;
-        VelocityY += (_constant1/MathF.Pow(distance, 2))*dT*cosB*k;
-        Debug.Log(_constant1 / MathF.Pow(distance, 2) * dT * cosB * k+","+distance);
+        
         if (distance < 0.2) {
             GameObject.Destroy(gameObject);
         }
->>>>>>> Stashed changes
-
+        
         Rigidbody.linearVelocity = new Vector2(_velocity.x*k, _velocity.y*k);
+
+        Debug.Log(_constant1 / MathF.Pow(distance, 2) * dT * cosB * k+","+distance);
         //Debug.Log(velocity +","+  currentX+ ","+ cosA); 
     }
 
