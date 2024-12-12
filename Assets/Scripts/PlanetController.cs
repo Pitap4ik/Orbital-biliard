043499ad7e48c1092bfb5c0c0913f16ecc830c21
Pitap4ik <potapodan0702@gmail.com>
2024-12-12 17:11:08 +0100

    colisions physics

--- Assets/Scripts/PlanetController.cs
+++ Assets/Scripts/PlanetController.cs
@@ -10,8 +10,8 @@ public class PlanetController : MonoBehaviour
     [SerializeField] private bool _isDraggable;
     [SerializeField] private float _kickPower;
     [SerializeField] private GameObject _pointOfGravitation;
-    [SerializeField] private bool Clockwise;
-    [SerializeField] private float _conservedEnergy;
+    [SerializeField] private bool _clockwise;
+    [SerializeField] private float _conservedEnergy; 
     private CanvasController _canvasController;
     private Vector3 _mousePosition;
     public Transform Transform { get; private set; }
@@ -31,7 +31,6 @@ public class PlanetController : MonoBehaviour
 
         if (_isCircularMotion){
             _velocity = GetCircularMotionVelocity(Transform.position, _constant1);
-            
         }
     }
 
@@ -56,8 +55,51 @@ public class PlanetController : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
+        float mOther = other.collider.attachedRigidbody.mass;
+        float mSelf = Rigidbody.mass;
+        Vector2 VelSelf = Rigidbody.linearVelocity;
+        Vector2 VelOther = other.collider.attachedRigidbody.linearVelocity;
+        Vector2 CoorOther = other.transform.position;
+        Vector2 CoorSelf = Transform.position;
+
+        Vector2 deltapos = new Vector2(CoorOther.x-CoorSelf.x, CoorOther.y-CoorSelf.y);
+
+        Vector2 VelSelfx1y1 = NormalAndPerpVelocity(VelSelf, deltapos);
+        Vector2 VelOtherx1y1 = NormalAndPerpVelocity(VelSelf, deltapos);
+
+        float Py1 = mOther * VelOtherx1y1.y + mSelf * VelSelfx1y1.y;
+
+        float E = (MathF.Pow(VelOtherx1y1.y, 2)+ MathF.Pow(VelOtherx1y1.x, 2)) * mOther / 2 + (MathF.Pow(VelSelfx1y1.y, 2) + MathF.Pow(VelSelfx1y1.x, 2)) * mSelf;
+
+        float ExAfter = (MathF.Pow(VelOtherx1y1.x, 2)) * mOther / 2 + (MathF.Pow(VelSelfx1y1.x, 2)) * mSelf;
+
+        float Edif = E - ExAfter;
+
+        float a = (mSelf/2+MathF.Pow(mSelf,2)/(2*mOther));
+
+        float b = mSelf * Py1 / mOther;
+
+        float c = MathF.Pow(Py1,2)/(2*mOther)-Edif;
+
+        float D = MathF.Pow(b, 2) - 4 * a * c;
+
+
+        if (MathF.Abs((-b + Mathf.Sqrt(D)) / (2 * a) - VelOtherx1y1.y) >= MathF.Abs((-b - Mathf.Sqrt(D)) / (2 * a) - VelOtherx1y1.y))
+        {
+            VelOtherx1y1.y = (-b + Mathf.Sqrt(D)) / (2 * a);
+
+        }
+        else
+        {
+            VelOtherx1y1.y = (-b - Mathf.Sqrt(D)) / (2 * a);
+        }
+
+
+        Rigidbody.linearVelocity = ReverseNormalAndPerpVelocity(VelOtherx1y1, deltapos);
+
         Debug.Log("A collider has made contact with the DoorObject Collider");
-        Rigidbody.AddForce(other.collider.attachedRigidbody.linearVelocity * _kickPower);
+        Debug.Log(VelOtherx1y1.y);
+        //Rigidbody.AddForce(other.collider.attachedRigidbody.linearVelocity * _kickPower);
     }
 
     void OnMouseDown()
@@ -85,6 +127,22 @@ public class PlanetController : MonoBehaviour
         return Camera.main.WorldToScreenPoint(transform.position);
     }
 
+    public Vector2 NormalAndPerpVelocity(Vector2 v,Vector2 deltapos)
+    {
+        float l = GetDistance(deltapos);
+        float Vx1=v.x*(deltapos.y/l)-v.y*(deltapos.x/l);
+        float Vy1 = v.x * (deltapos.x / l) + v.y * (deltapos.y / l);
+        return new Vector2(Vx1, Vy1);// Vx1 is pependicular 
+    }
+
+    public Vector2 ReverseNormalAndPerpVelocity(Vector2 v, Vector2 deltapos)
+    {
+        float l = GetDistance(deltapos);
+        float Vx1 = v.x * (deltapos.y / l) + v.y * (deltapos.x / l);
+        float Vy1 = - v.x * (deltapos.x / l) + v.y * (deltapos.y / l);
+        return new Vector2(Vx1, Vy1); 
+    }
+
     public float GetDistance(Vector2 objectPosition){
         return MathF.Sqrt(MathF.Pow(objectPosition.x, 2) + MathF.Pow(objectPosition.y, 2));
     }
@@ -99,7 +157,7 @@ public class PlanetController : MonoBehaviour
         float velocityX = -MathF.Sqrt(constant1/distance) * position.y / distance;
         float velocityY = MathF.Sqrt(constant1/distance) * position.x / distance;
 
-        if (Clockwise) { velocityX *= -1; velocityY *= -1; }
+        if (_clockwise) { velocityX *= -1; velocityY *= -1; }
 
         return new Vector2(velocityX, velocityY);
     }
